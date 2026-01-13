15d8916a9fd99bddb849f1d4bbfd1a4e4831e507
saigor33 <saigor33@yandex.ru>
2026-01-13 23:06:30 +0300

    StateGraphVisualization: Refactor. Extract CodeAnalysis class

--- ExampleUnityProject/Assets/Scripts/Test.cs
+++ ExampleUnityProject/Assets/Scripts/Test.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Frameworks.StateMachine.StateGraphVisualizer;
 using UnityEngine;
 
 public class Test : MonoBehaviour
@@ -44,79 +40,8 @@ public class Test : MonoBehaviour
            .SelectMany(t => t)
            .ToArray();
 
-        AnalyzeCode(inheritSelectedBaseStateTypes, inheritSelectedTransitionTypes);
+        CodeAnalyzer.Analyze(inheritSelectedBaseStateTypes, inheritSelectedTransitionTypes);
 
         UnityEngine.Debug.Log($"#{UnityEngine.Time.frameCount}: Done");
-    }
-
-    static void AnalyzeCode(Type[] inheritBaseStateTypes, Type[] inheritBaseTransitionTypes)
-    {
-        string rootDirectoryPath =
-            @"C:\MyFolder\Projects\Frameworks\StateMachine\ExampleUnityProject\Assets\Scripts\Match";
-
-        string[] cSharpFilePaths = Directory.GetFiles(rootDirectoryPath, "*.cs", SearchOption.AllDirectories);
-        string sourceCode = string.Join("\n", cSharpFilePaths.Select(File.ReadAllText));
-
-        SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
-        var compilation = CSharpCompilation.Create("MyCompilation", new[] { syntaxTree });
-        SemanticModel semanticModel = compilation.GetSemanticModel(syntaxTree);
-        SyntaxNode syntaxRoot = syntaxTree.GetRoot();
-
-        Dictionary<ISymbol, HashSet<INamedTypeSymbol>> creationStateSourcesByState =
-            inheritBaseStateTypes
-               .Select(t => compilation.GetTypeByMetadataName(t.FullName))
-               .ToDictionary(
-                    symbol => symbol,
-                    _ => new HashSet<INamedTypeSymbol>(SymbolEqualityComparer.Default),
-                    SymbolEqualityComparer.Default
-                );
-
-        Dictionary<ISymbol, HashSet<INamedTypeSymbol>> creationTransitionSourcesByTransition =
-            inheritBaseTransitionTypes
-               .Select(t => compilation.GetTypeByMetadataName(t.FullName))
-               .ToDictionary(
-                    symbol => symbol,
-                    _ => new HashSet<INamedTypeSymbol>(SymbolEqualityComparer.Default),
-                    SymbolEqualityComparer.Default
-                );
-
-        IEnumerable<ObjectCreationExpressionSyntax> objectCreationExpressionSyntaxes = syntaxRoot
-           .DescendantNodes()
-           .OfType<ObjectCreationExpressionSyntax>();
-
-        foreach (ObjectCreationExpressionSyntax objectCreationExpressionSyntax in objectCreationExpressionSyntaxes)
-        {
-            TypeInfo typeInfo = semanticModel.GetTypeInfo(objectCreationExpressionSyntax);
-            ITypeSymbol typeInfoTypeSymbol = typeInfo.Type;
-
-            if (typeInfoTypeSymbol == null)
-            {
-                continue;
-            }
-
-            if (creationStateSourcesByState.TryGetValue(typeInfoTypeSymbol,
-                out HashSet<INamedTypeSymbol> creationStateSources))
-            {
-                ClassDeclarationSyntax sourceClassDeclarationSyntax =
-                    GetClassDeclarationSyntax(objectCreationExpressionSyntax);
-                INamedTypeSymbol namedTypeSymbol = semanticModel.GetDeclaredSymbol(sourceClassDeclarationSyntax);
-                creationStateSources.Add(namedTypeSymbol);
-            }
-            else if (creationTransitionSourcesByTransition.TryGetValue(typeInfoTypeSymbol,
-                out HashSet<INamedTypeSymbol> creationTransitionSources))
-            {
-                ClassDeclarationSyntax sourceClassDeclarationSyntax =
-                    GetClassDeclarationSyntax(objectCreationExpressionSyntax);
-                INamedTypeSymbol namedTypeSymbol = semanticModel.GetDeclaredSymbol(sourceClassDeclarationSyntax);
-                creationTransitionSources.Add(namedTypeSymbol);
-            }
-        }
-
-        UnityEngine.Debug.Log($"#{UnityEngine.Time.frameCount}:test ");
-    }
-
-    static ClassDeclarationSyntax GetClassDeclarationSyntax(SyntaxNode syntaxNode)
-    {
-        return syntaxNode.Parent as ClassDeclarationSyntax ?? GetClassDeclarationSyntax(syntaxNode.Parent);
     }
 }
