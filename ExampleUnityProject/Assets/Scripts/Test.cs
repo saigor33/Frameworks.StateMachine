d50cd5f1489da1895167955e9906c230f06cebaf
saigor33 <saigor33@yandex.ru>
2026-01-13 18:49:56 +0300

    StateGraphVisualization: Get all inherit state and transition types

--- ExampleUnityProject/Assets/Scripts/Test.cs
+++ ExampleUnityProject/Assets/Scripts/Test.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using UnityEngine;
 
 public class Test : MonoBehaviour
@@ -11,6 +11,25 @@ public class Test : MonoBehaviour
     [ContextMenu("TestAnalysisCode")]
     public void TestAnalysisCode()
     {
+        Type baseStateType = typeof(Frameworks.StateMachine.BaseState<>);
+        Type baseTransitionType = typeof(Frameworks.StateMachine.BaseTransition<>);
+        Type baseTransitionWithContextType = typeof(Frameworks.StateMachine.BaseTransition<,>);
+
+        Type[] allAbstractClassTypes =
+            AppDomain.CurrentDomain.GetAssemblies()
+               .SelectMany(assembly => assembly.GetTypes())
+               .Where(type => type.IsClass && type.IsAbstract).ToArray();
+
+        Type[] inheritBaseStateTypes = allAbstractClassTypes
+           .Where(abstractClassType => IsInheritType(abstractClassType, baseStateType))
+           .ToArray();
+
+        Type[] inheritTransitionTypes = allAbstractClassTypes
+           .Where(abstractClassType => IsInheritType(abstractClassType, baseTransitionType) ||
+                IsInheritType(abstractClassType, baseTransitionWithContextType))
+           .ToArray();
+
+
         string rootDirectoryPath =
             @"C:\MyFolder\Projects\Frameworks\StateMachine\ExampleUnityProject\Assets\Scripts\Match";
 
@@ -24,5 +43,12 @@ public class Test : MonoBehaviour
         IEnumerable<SyntaxNode> syntaxNodes = syntaxRoot.DescendantNodes();
 
         UnityEngine.Debug.Log($"#{UnityEngine.Time.frameCount}: Done");
+    }
+
+    static bool IsInheritType(Type abstractClassType, Type baseStateType)
+    {
+        return abstractClassType.BaseType != null &&
+            abstractClassType.BaseType.IsGenericType &&
+            abstractClassType.BaseType.GetGenericTypeDefinition() == baseStateType;
     }
 }
