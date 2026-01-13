505eadbf89ea1ce89012821850ba16c80d0b2b63
saigor33 <saigor33@yandex.ru>
2026-01-13 18:19:14 +0300

    ExampleUnityProject: Loaded syntax tree

--- ExampleUnityProject/Assets/Scripts/Test.cs
+++ ExampleUnityProject/Assets/Scripts/Test.cs
@@ -1,10 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using UnityEngine;
 
 public class Test : MonoBehaviour
 {
     [ContextMenu("TestAnalysisCode")]
     public void TestAnalysisCode()
     {
-        UnityEngine.Debug.Log($"#{UnityEngine.Time.frameCount}: test");
+        string rootDirectoryPath =
+            @"C:\MyFolder\Projects\Frameworks\StateMachine\ExampleUnityProject\Assets\Scripts\Match";
+
+        string[] cSharpFilePaths = Directory.GetFiles(rootDirectoryPath, "*.cs", SearchOption.AllDirectories);
+        string sourceCode = string.Join("\n", cSharpFilePaths.Select(File.ReadAllText));
+
+        SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+        var compilation = CSharpCompilation.Create("MyCompilation", new[] { syntaxTree });
+
+        SyntaxNode syntaxRoot = syntaxTree.GetRoot();
+        IEnumerable<SyntaxNode> syntaxNodes = syntaxRoot.DescendantNodes();
+
+        UnityEngine.Debug.Log($"#{UnityEngine.Time.frameCount}: Done");
     }
 }
