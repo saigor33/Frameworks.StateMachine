1d7a9cccd1a8acf4072f1aeccad767c7d672792b
saigor33 <saigor33@yandex.ru>
2026-01-13 17:33:16 +0300

    ExampleUnityProject: Add start method Analysis code

---
+++ ExampleUnityProject/Assets/Scripts/Test.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [ContextMenu("TestAnalysisCode")]
+    public void TestAnalysisCode()
+    {
+        UnityEngine.Debug.Log($"#{UnityEngine.Time.frameCount}: test");
+    }
+}
