dd842308444470dbd3aa8780e305236939a80efe
saigor33 <saigor33@yandex.ru>
2026-01-19 16:20:17 +0300

    StateGraphVisualization: Refactor. Rename GraphvizStateGraphGenerator and extract in own file

---
+++ ExampleUnityProject/Assets/Scripts/GraphvizStateGraphGenerator.cs
@@ -0,0 +1,191 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Frameworks.StateMachine.StateGraphVisualizer
+{
+    static class GraphvizStateGraphGenerator
+    {
+        public class Result
+        {
+            public string stateGraphText;
+            public string stateGraphWithTransitionsText;
+        }
+
+        public static Result Build(CodeAnalyzer.Result codeAnalyzer)
+        {
+            string commonSubname = GetCommonSubstring(codeAnalyzer.fromStateToTransitionByTransition);
+            commonSubname = GetCommonSubstring(codeAnalyzer.fromTransitionToStateByState, commonSubname);
+            commonSubname = GetCommonSubstring(codeAnalyzer.fromOtherSourceToTransitionByTransition, commonSubname);
+            commonSubname = GetCommonSubstring(codeAnalyzer.fromOtherSourceToStateByState, commonSubname);
+
+            var stringBuilder = new StringBuilder();
+
+            var fromStateToTransitionByState = new Dictionary<string, HashSet<string>>();
+            foreach ((string transitionId, HashSet<string> stateIds) in codeAnalyzer.fromStateToTransitionByTransition)
+            {
+                foreach (string stateId in stateIds)
+                {
+                    if (fromStateToTransitionByState.TryGetValue(stateId, out HashSet<string> value))
+                    {
+                        value.Add(transitionId);
+                    }
+                    else
+                    {
+                        fromStateToTransitionByState.Add(stateId, new HashSet<string>(new[] { transitionId }));
+                    }
+                }
+            }
+
+            var createdTransitionIds = new HashSet<string>();
+            foreach ((string stateId, HashSet<string> transitionIds) in fromStateToTransitionByState)
+            {
+                createdTransitionIds.UnionWith(transitionIds);
+                stringBuilder.AppendLine(CreateStateWithTransitionsNode(stateId, transitionIds));
+            }
+
+            foreach (string stateId in codeAnalyzer.states)
+            {
+                bool isStateNodeCreated = fromStateToTransitionByState.ContainsKey(stateId);
+                if (!isStateNodeCreated)
+                {
+                    string notUsedStateNode =
+                        CreateStateWithTransitionsNode(stateId, transitionIds: new HashSet<string>());
+                    stringBuilder.AppendLine(notUsedStateNode);
+                }
+            }
+
+            foreach (string transitionId in codeAnalyzer.transitions)
+            {
+                bool isTransitionNodeCreated = createdTransitionIds.Contains(transitionId);
+                if (!isTransitionNodeCreated)
+                {
+                    string notUsedTransitionNode = CreateTransitionNode(transitionId, GraphvizFormatter.Color.Yellow);
+                    stringBuilder.AppendLine(notUsedTransitionNode);
+                }
+            }
+
+            foreach ((string stateId, HashSet<string> transitionIds) in codeAnalyzer.fromTransitionToStateByState)
+            {
+                foreach (string transitionId in transitionIds)
+                {
+                    stringBuilder.AppendLine(GraphvizFormatter.JoinNodes(transitionId, stateId));
+                }
+            }
+
+            HashSet<string> allOtherSourceIds = new Dictionary<string, HashSet<string>>()
+               .Concat(codeAnalyzer.fromOtherSourceToStateByState)
+               .Concat(codeAnalyzer.fromOtherSourceToTransitionByTransition)
+               .SelectMany(kv => kv.Value)
+               .ToHashSet();
+
+            foreach (string otherSourceId in allOtherSourceIds)
+            {
+                stringBuilder.AppendLine(
+                    GraphvizFormatter.FormatNode(
+                        nodeId: otherSourceId,
+                        nodeLabel: GetSourceName(otherSourceId),
+                        color: GraphvizFormatter.Color.Yellow
+                    ));
+            }
+
+            foreach ((string stateId, HashSet<string> otherSourceIds) in codeAnalyzer.fromOtherSourceToStateByState)
+            {
+                foreach (string otherSourceId in otherSourceIds)
+                {
+                    stringBuilder.Append(GraphvizFormatter.JoinNodes(otherSourceId, stateId));
+                }
+            }
+
+            foreach ((string transitionId, HashSet<string> otherSourceIds) in codeAnalyzer
+               .fromOtherSourceToTransitionByTransition)
+            {
+                foreach (string otherSourceId in otherSourceIds)
+                {
+                    stringBuilder.Append(GraphvizFormatter.JoinNodes(otherSourceId, transitionId));
+                }
+            }
+
+            return new Result
+            {
+                stateGraphText = "",
+                stateGraphWithTransitionsText =
+                    GraphvizFormatter.FormatDigraph("Root", "", stringBuilder.ToString())
+            };
+        }
+
+        static string CreateStateWithTransitionsNode(string stateId, HashSet<string> transitionIds)
+        {
+            var stateStringBuild = new StringBuilder();
+
+            stateStringBuild.AppendLine(GraphvizFormatter.FormatNode(stateId, "State"));
+
+            foreach (string transitionId in transitionIds)
+            {
+                stateStringBuild.AppendLine(CreateTransitionNode(transitionId));
+                stateStringBuild.AppendLine(GraphvizFormatter.JoinNodes(stateId, transitionId));
+            }
+
+            return GraphvizFormatter.FormatSubgraph(GetSourceName(stateId), nodes: $"{stateStringBuild}");
+        }
+
+        static string CreateTransitionNode(string transitionId, string color = GraphvizFormatter.Color.White)
+        {
+            return GraphvizFormatter.FormatNode(transitionId,
+                nodeLabel: GetSourceName(transitionId),
+                color: color,
+                shapeType: GraphvizFormatter.ShapeType.Rect);
+        }
+
+        static string GetSourceName(string sourceId)
+        {
+            int lastIndexOf = sourceId.LastIndexOf('.');
+
+            if (lastIndexOf == -1)
+            {
+                return sourceId;
+            }
+
+            return sourceId.Substring(lastIndexOf + 1);
+        }
+
+        static string GetCommonSubstring(Dictionary<string, HashSet<string>> allSourceToSources,
+            string commonSubname = null)
+        {
+            foreach ((string fromSourceId, HashSet<string> toSourceIds) in allSourceToSources)
+            {
+                commonSubname = commonSubname == null
+                    ? fromSourceId
+                    : GetCommonSubstring(commonSubname, fromSourceId);
+
+                foreach (string toSourceId in toSourceIds)
+                {
+                    commonSubname = GetCommonSubstring(commonSubname, toSourceId);
+                }
+            }
+
+            return commonSubname;
+        }
+
+        static string GetCommonSubstring(string str1, string str2)
+        {
+            int? lastMatchSubstringIndex = null;
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (i >= str2.Length
+                    || str1[i] != str2[i])
+                {
+                    break;
+                }
+
+                lastMatchSubstringIndex = i;
+            }
+
+            int commonSubstringLenght = lastMatchSubstringIndex.HasValue
+                ? lastMatchSubstringIndex.Value + 1
+                : 0;
+
+            return str1.Substring(0, commonSubstringLenght);
+        }
+    }
+}
